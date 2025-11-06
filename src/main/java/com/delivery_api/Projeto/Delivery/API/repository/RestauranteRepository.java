d92f30c93bca33162e58d48bb0db3dbcfeabe74d
Antonio Silva <antonio.h.torres21@gmail.com>
2025-11-06 00:47:46 -0300

    Services parcialmente criada

--- src/main/java/com/delivery_api/Projeto/Delivery/API/repository/RestauranteRepository.java
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/repository/RestauranteRepository.java
@@ -1,17 +1,15 @@
 package com.delivery_api.Projeto.Delivery.API.repository;
 //Buscar por nome, categoria, ativos, ordenação por avaliação
 
-import com.delivery_api.Projeto.Delivery.API.entity.Cliente;
 import com.delivery_api.Projeto.Delivery.API.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RestaurenteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
     //Busca restaurante por nome
     Optional<Restaurante> findByNome(String nome);
 
@@ -23,4 +21,7 @@ public interface RestaurenteRepository extends JpaRepository<Restaurante, Long> 
 
     //Busca por restaurantes e ordena por avaliação
     List<Restaurante> findAllByOrderByAvaliacaoAsc();
+
+    // Verificar se restaurante já existe
+    boolean existsByNome(String nome);
 }
--- src/main/java/com/delivery_api/Projeto/Delivery/API/service/PedidoService.java
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/service/PedidoService.java
@@ -1,4 +1,42 @@
 package com.delivery_api.Projeto.Delivery.API.service;
+import com.delivery_api.Projeto.Delivery.API.entity.Pedido;
+import com.delivery_api.Projeto.Delivery.API.entity.Restaurante;
+import com.delivery_api.Projeto.Delivery.API.repository.PedidoRepository;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class PedidoService {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    /*
+    @Column(name = "numero_pedido")
+    private String numeroPedido;
+
+    @Column(name = "data_pedido")
+    private LocalDateTime dataPedido;
+
+    @Column(name = "cliente_id")
+    private Long clienteId;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id")
+    private Restaurante restaurante;
+
+
+     */
+
+
+
 }
